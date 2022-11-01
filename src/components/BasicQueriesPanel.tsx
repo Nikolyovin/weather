bc16d8e788a0fd75ef51e5929fde2c89fbf1dc70
Nikolyovin <pulya0763@gmail.com>
2022-11-01 20:06:13 +0300

    minimal adaptiv

--- src/components/BasicQueriesPanel.tsx
+++ src/components/BasicQueriesPanel.tsx
@@ -1,12 +1,12 @@
 
 const BasicQueriesPanel = () => {
   return (
-    <div className="flex flex-col py-16 px-12">
-        <span className="basicQueriesPanel-item">Ryazan</span>
-        <span className="basicQueriesPanel-item">Omsk</span>
-        <span className="basicQueriesPanel-item">Novosibirsk</span>
-        <span className="basicQueriesPanel-item">Vladivostok</span>
-        <span className="basicQueriesPanel-item">Irkutsk</span>
+    <div className="flex flex-col py-16 px-12 max-2xl:py-6 max-2xl:px-4">
+      <span className="basicQueriesPanel-item max-2xl:mb-1">Ryazan</span>
+      <span className="basicQueriesPanel-item max-2xl:mb-1">Omsk</span>
+      <span className="basicQueriesPanel-item max-2xl:mb-1">Novosibirsk</span>
+      <span className="basicQueriesPanel-item max-2xl:mb-1">Vladivostok</span>
+      <span className="basicQueriesPanel-item max-2xl:mb-1">Irkutsk</span>
     </div>
   )
 }
